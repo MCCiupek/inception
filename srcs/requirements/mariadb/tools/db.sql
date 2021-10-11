96a2df07bc1589f862aff037782452a5ab5d15da
MCC <marieclemence.c@gmail.com>
2021-10-11 18:19:25 +0200

    mariadb config

--- srcs/requirements/mariadb/tools/db.sql
+++ srcs/requirements/mariadb/tools/db.sql
@@ -2,5 +2,4 @@ CREATE DATABASE '${MYSQL_DATABASE}';
 CREATE USER '${MYSQL_USER}'@'%';
 SET password FOR '${MYSQL_USER}'@'%' = password('${MYSQL_PASSWORD}');
 GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-update mysql.user set plugin=' ' where user='root';
 FLUSH PRIVILEGES;
