5ba3def11f0eb32cf3dd67ada9b158a9583e7749
MCC <marieclemence.c@gmail.com>
2021-10-22 09:45:30 +0200

    fix env

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -12,9 +12,8 @@ then
 	echo "Database already exists"
 else
 
-# Set root option so that connexion without root password is not possible
-
-mysql_secure_installation <<_EOF_
+    # Set root option so that connexion without root password is not possible
+    mysql_secure_installation <<_EOF_
 Y
 root42
 root42
@@ -24,15 +23,14 @@ Y
 Y
 _EOF_
 
-#Add a root user on 127.0.0.1 to allow remote connexion
+    #Add a root user on 127.0.0.1 to allow remote connexion
+    echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
-	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+    #Create database and user for wordpress
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
-#Create database and user for wordpress
-	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-
-#Import database
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/db.sql
+    #Import database
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/db.sql
 
 fi
 
