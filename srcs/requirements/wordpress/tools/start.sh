0658e54759dc626b9803870e70da979d75da5d9d
MCC <marieclemence.c@gmail.com>
2021-10-25 09:58:26 +0200

    fix wordpress config

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -9,24 +9,24 @@ else
 	tar -xzvf latest.tar.gz
 	rm -rf latest.tar.gz
     #mkdir /var/www
-    mv -f wordpress /var/www/
+    mv -f wordpress /var/www/wordpress
     chmod 755 -R /var/www/wordpress
     chown -R www-data:www-data /var/www/wordpress
 
 	#Update configuration file
     rm -rf /var/www/wordpress/wp-config-sample.php
-    mv ../conf/wp-config.php /var/www/wordpress
+    mv ../conf/wp-config.php /var/www/wordpress/wp-config.php
 
 	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
-	mv ../conf/www.conf /etc/php/7.3/fpm/pool.d/
+	mv ../conf/www.conf /etc/php/7.3/fpm/pool.d/www.conf
 
 	#Inport env variables in the config file
-	cd /var/www/wordpress
-	sed -i "s/username_here/$WP_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$WP_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$WP_DB_HOST/g" wp-config-sample.php
-	sed -i "s/database_name_here/$WP_DB_NAME/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
+	# cd /var/www/wordpress
+	# sed -i "s/username_here/$WP_USER/g" wp-config-sample.php
+	# sed -i "s/password_here/$WP_PASSWORD/g" wp-config-sample.php
+	# sed -i "s/localhost/$WP_DB_HOST/g" wp-config-sample.php
+	# sed -i "s/database_name_here/$WP_DB_NAME/g" wp-config-sample.php
+	# mv wp-config-sample.php wp-config.php
 fi
 
 exec "$@"
