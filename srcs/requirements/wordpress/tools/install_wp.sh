1c508b52f3e1c03010e92a098d80d03da0bea501
MCC <marieclemence.c@gmail.com>
2021-10-26 17:29:18 +0200

    wordpress: Check whether wp is installed or not

---
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -f ./wordpress/wp-config.php ]
+then
+	echo "Wordpress is already downloaded"
+else
+    cd /var/www/
+	#Download wordpress
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzvf latest.tar.gz
+	rm -rf latest.tar.gz
+
+fi
+
+exec "$@"
