78844461801a02978b0869c78579ebf1fd437618
MCC <marieclemence.c@gmail.com>
2021-10-26 10:02:50 +0200

    mariadb/create_db.sh: ping mysqladmin

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -2,7 +2,7 @@
 
 echo "Database initialization"
 
-mariadbd &
+mysqld &
 
 if ! mysqladmin --wait=30 ping; then
 	printf "Could not ping mariadb for 30 seconds, runtime configuration is not possible.\n"
@@ -15,8 +15,8 @@ fi
 # echo "wait 15s"
 # sleep 15
 
-echo "ajout de la db"
-sleep 2
+echo "Building database"
+# sleep 2
 mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS ${WP_USER}@'%' IDENTIFIED BY '${WP_USER_PASSWORD}';"
 mysql -e "CREATE USER IF NOT EXISTS ${WP_ADMIN}@'%' IDENTIFIED BY '${WP_ADMIN_PASSWORD}';"
@@ -24,11 +24,10 @@ mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${WP_ADMIN}@'%' IDENTIFIED BY '${WP_ADM
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${WP_USER}@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
-echo "database shutdown"
-sleep 2
+echo "Database shutdown"
+# sleep 2
 mysqladmin shutdown
 
-echo "database restarting"
-sleep 2
-
+echo "Restarting database"
+# sleep 2
 exec mysqld -u root
