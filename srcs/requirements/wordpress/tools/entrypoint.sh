778a8cb02f20e5cb39dc8e99fcc391a9bd2930e5
MCC <marieclemence.c@gmail.com>
2021-10-26 17:32:16 +0200

    wordpress: Check whether wp is installed or not

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo "Install wp"
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
-chmod +x /usr/local/bin/wp
-wp core download --path=/var/www --locale=fr_FR
+# echo "Install wp"
+# wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
+# chmod +x /usr/local/bin/wp
+# wp core download --path=/var/www --locale=fr_FR
 
-echo "Create DB"
-wp config create --dbname="$WP_DB_NAME" --dbuser="$WP_USER" --dbpass="$WP_USER_PASSWORD" --dbhost="$WP_DB_HOST" --path=/var/www/ --dbprefix="$WP_TABLE_PREFIX"
-wp core install --url="$DOMAIN_NAME" --title="$WP_NAME" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN@email.com" --skip-email --path=/var/www/
+# echo "Create DB"
+# wp config create --dbname="$WP_DB_NAME" --dbuser="$WP_USER" --dbpass="$WP_USER_PASSWORD" --dbhost="$WP_DB_HOST" --path=/var/www/ --dbprefix="$WP_TABLE_PREFIX"
+# wp core install --url="$DOMAIN_NAME" --title="$WP_NAME" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN@email.com" --skip-email --path=/var/www/
 
-echo "Create user"
-wp user create $WP_USER $WP_USER@email.com --role="author" --user_pass="$WP_USER_PASSWORD" --path="/var/www"
+# echo "Create user"
+# wp user create $WP_USER $WP_USER@email.com --role="author" --user_pass="$WP_USER_PASSWORD" --path="/var/www"
 
 echo "Starting php-fpm7.3..."
 /usr/sbin/php-fpm7.3 -F
--- srcs/requirements/wordpress/tools/install_wp.sh
+++ srcs/requirements/wordpress/tools/install_wp.sh
@@ -5,7 +5,9 @@ then
 	echo "Wordpress is already downloaded"
 else
     cd /var/www/
+    
 	#Download wordpress
+    echo "Install Wordpress"
 	wget https://wordpress.org/latest.tar.gz
 	tar -xzvf latest.tar.gz
 	rm -rf latest.tar.gz
