f72f70572cd8cad14e3d75d8deb5b62621ce54f3
MCC <marieclemence.c@gmail.com>
2021-10-26 17:58:03 +0200

    wp: add database config in entrypoint.sh

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -5,7 +5,7 @@ wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -
 chmod +x /usr/local/bin/wp
 wp core download --path=/var/www --locale=fr_FR
 
-echo "Create DB : $WP_DB_NAME -"
+echo "Create DB : $WP_DB_NAME"
 wp config create --dbname="$WP_DB_NAME" --dbuser="$WP_USER" --dbpass="$WP_USER_PASSWORD" --dbhost="$WP_DB_HOST" --path=/var/www/ --dbprefix="$WP_TABLE_PREFIX"
 wp core install --url="$DOMAIN_NAME" --title="$WP_NAME" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN@email.com" --skip-email --path=/var/www/
 
