c7a84263f3e22a08e8a79f8b46f89bdb860ffaf4
MCC <marieclemence.c@gmail.com>
2021-10-26 10:16:41 +0200

    wp/Dockerfile: change entrypoint

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -9,14 +9,7 @@ if ! mysqladmin --wait=30 ping; then
 	exit 1
 fi
 
-# mysqld_safe &
-# sleep 2
-
-# echo "wait 15s"
-# sleep 15
-
 echo "Building database"
-# sleep 2
 mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS ${WP_USER}@'%' IDENTIFIED BY '${WP_USER_PASSWORD}';"
 mysql -e "CREATE USER IF NOT EXISTS ${WP_ADMIN}@'%' IDENTIFIED BY '${WP_ADMIN_PASSWORD}';"
@@ -25,9 +18,7 @@ mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${WP_USER}@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
 echo "Database shutdown"
-# sleep 2
 mysqladmin shutdown
 
 echo "Restarting database"
-# sleep 2
 exec mysqld -u root
---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Starting php-fpm7.3..."
+/usr/sbin/php-fpm7.3 -F
