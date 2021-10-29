04a7a5933aaf297b01e647f4160ec5d817f72a71
MCC <marieclemence.c@gmail.com>
2021-10-29 11:49:59 +0200

    wp: add user

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -17,8 +17,8 @@ mysql -e "CREATE USER IF NOT EXISTS ${WP_USER}@'%' IDENTIFIED BY '${WP_USER_PASS
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${WP_ADMIN}@'%' IDENTIFIED BY '${WP_ADMIN_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${WP_USER}@'%';"
 mysql -e "INSERT INTO wordpress.wp_users 
-(user_login,user_pass,user_email) 
-VALUES ('${WP_USER}'@'%','${WP_USER_PASSWORD}','${WP_USER}@42.fr');"
+(user_login,user_pass,user_nicename,user_email,user_url,user_registered,user_activation_key,user_status,display_name) 
+VALUES ('${WP_USER}'@'%',MD5('${WP_USER_PASSWORD}'),'${WP_USER}','${WP_USER}@42.fr','https://${DOMAIN_NAME}','2021-10-27','',0,'${WP_USER}');"
 mysql -e "FLUSH PRIVILEGES;"
 
 echo "Database shutdown"
