0c3a757ff2b803a78467a903abc56a7b250f4f14
MCC <marieclemence.c@gmail.com>
2021-10-29 11:12:26 +0200

    wp: add user

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
+chmod +x /usr/local/bin/wp
+
+wp config create --dbname="$WP_DB_NAME" \
+    --dbuser="$WP_USER" \
+    --dbpass="$WP_USER_PASSWORD" \
+    --dbhost="$WP_DB_HOST" \
+    --path=/var/www/ \
+    --dbprefix="$WP_TABLE_PREFIX"
+
 echo "Starting Nginx..."
 nginx -g "daemon off;"
