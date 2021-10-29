a6f3f6cbd041dbd3a180467379d8ef450b3c3336
MCC <marieclemence.c@gmail.com>
2021-10-29 11:21:14 +0200

    wp: add user

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -15,7 +15,7 @@
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
 chmod +x /usr/local/bin/wp
 
-wp config create --dbname="$WP_DB_NAME" \
+sudo -u $WP_USER -i wp config create --dbname="$WP_DB_NAME" \
     --dbuser="$WP_USER" \
     --dbpass="$WP_USER_PASSWORD" \
     --dbhost="$WP_DB_HOST" \
